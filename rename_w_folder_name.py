44d1ed6a57c8209d250b83cb536f354d037db244
bzerath <bzerath]@hotmail.com>
2016-12-08 11:29:00 +0100

    Add rename_w_folder_name

---
+++ rename_w_folder_name.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals  # Everything is UTF-8
+import os
+
+path_folder = "F:\\Users\\bzerath\\Documents\\"
+
+if __name__ == "__main__":
+    print (os.listdir(path_folder))
+    folders = os.listdir(path_folder)
+    for folder in folders:
+        for fichier in os.listdir(os.path.join(path_folder, folder)):
+            os.rename(os.path.join(path_folder, folder, fichier),
+                      os.path.join(path_folder, folder+"_"+fichier))
+            # Cancel :
+            # len_entete = len(folder + "_")
+            # os.rename(os.path.join(path_folder, folder, fichier),
+            #           os.path.join(path_folder, folder, fichier[len_entete:]))
+
+    
